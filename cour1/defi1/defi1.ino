c8d57d8eaa8f9533b043b7154ec71ae6dfcc58fb
Nicolas Prigge <priggen@educ.college-montreal.qc.ca>
2023-10-16 15:25:15 -0400

    debut dune anne

---
+++ cour1/defi1/defi1.ino
@@ -0,0 +1,14 @@
+byte LED = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(250);
+  digitalWrite(LED, LOW);
+  delay(250);
+}
