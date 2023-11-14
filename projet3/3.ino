555657be9d7f44f2015a9bcd1764f28b2da1dcee
steiyve <94018684+steiyve@users.noreply.github.com>
2023-11-14 16:14:53 -0500

    Update 3.ino

--- projet3/3.ino
+++ projet3/3.ino
@@ -5,10 +5,9 @@ byte modification = 5;
 const byte delay_time = 250;
 char choix = 0;
 
+// fonction qui prend un input
 void input(){
-  choix = Serial.parseInt();
-  Serial.println("votre choix");
-  Serial.println(choix);
+  choix = Serial.parseInt();          // User input(int) -> choix      
 }
 
 void setup() {
@@ -19,15 +18,16 @@ void setup() {
 }
 
 void loop() {
+  // allumer une led
   analogWrite(LED, light_amount);
-  
-  input();
-  if (choix == 1){
-    digitalWrite(green, HIGH);
-  }
 
-  else if (choix == 2){
-    digitalWrite(green, LOW);
+  //prendre linput et checker sa valeur
+  input();
+  switch (choix){
+    case 1:
+      digitalWrite(green, HIGH);                               // allumer la led
+    case 2:
+      digitalWrite(green, LOW);                                // fermer la led
   }
 
   // bloc de code qui donne une valeur random a modification
