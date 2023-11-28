ecf02cc5c38b1726dbeae5fc8cc90658bfa5e495
nicolas <priggen@educ.college-montreal.qc.ca>
2023-11-28 15:11:12 -0500

    le code est en vie suave moi

--- projet4/projet4.ino
+++ projet4/projet4.ino
@@ -1,28 +1,55 @@
+//definition des pin
 const byte led = 11;
 const byte button = 2;
-byte button_state = 0;
 
-void use_button() {
-  button_state = digitalRead(button);
+
+
+//=======================================================//
+//==================== FONCTIONS ========================//
+//=======================================================//
+//         fonction qui lit l'etat du bouton             //
+//      et qui l'assigne a la variable button state      //
+//=======================================================//
+byte use_button() 
+{
+  return digitalRead(button);
 }
 
+//=======================================================//
+//==================== FONCTIONS ========================//
+//=======================================================//
+//         fonction qui allume ou eteint la led          //
+//      en fonction de la valeur de button_state(param)  //
+//=======================================================//
+void on_off(byte button_state) 
+{                                  
+  switch(button_state) 
+  { 
+    case 1:                                        // si button_state = 1                   
+      Serial.println(button_state);                // on affiche button_state
+      digitalWrite(led, HIGH);                     // on allume la led
 
-void on_off() {
-  use_button();
-  switch(button_state) {
-    case 1:
-      Serial.println("ON");
-    case 0:
-      Serial.println("OFF");
+    case 0:                                       // si button_state = 0
+      Serial.println(button_state);               // on affiche button_state
+      digitalWrite(led, LOW);                     // on eteint la led        
+
+    default:                                      // si button_state != 0 ou 1
+      digitalWrite(led, LOW);                     // on eteint la led
   }
 }
-void setup() {
+void setup() 
+{
+  // definition des pin
   Serial.begin(9600);
   pinMode(button,INPUT);
   pinMode(led,OUTPUT);
 }
 
-void loop() {
-  on_off();
-
+void loop() 
+{
+  //definition des variables
+  byte button_state = use_button();
+  
+  //utilisation des fonctions
+  on_off(button_state);
 }
