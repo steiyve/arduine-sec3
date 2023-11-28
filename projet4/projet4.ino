cbb7d29ec8550e4206dbd26088d420ea81f18e5e
Gabriel Foriel-Fusier <forielfusierg@educ.college-montreal.qc.ca>
2023-11-28 14:39:34 -0500

    dawdA

--- projet4/projet4.ino
+++ projet4/projet4.ino
@@ -1,10 +1,28 @@
+const byte led = 11;
+const byte button = 2;
+byte button_state = 0;
 
+void use_button() {
+  button_state = digitalRead(button);
+}
+
+
+void on_off() {
+  use_button();
+  switch(button_state) {
+    case 1:
+      Serial.println("ON");
+    case 0:
+      Serial.println("OFF");
+  }
+}
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(button,INPUT);
+  pinMode(led,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  on_off();
 
 }
